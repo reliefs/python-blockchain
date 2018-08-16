2bbda6b56b1eaa4e1fc61ae1848ea30795cd567a
reliefs <6hourapp@gmail.com>
2018-08-16 05:27:54 +0000

    comments

--- blockchain.py
+++ blockchain.py
@@ -1,18 +1,31 @@
+# Intializing our blockchain list
 blockchain = []
 
 def get_last_blockchain_value():
+    """ Returns the last value of the current blockchain """
     return blockchain[-1]
 
 def add_value(transaction_amount, last_transaction=[1]):
+    """ Append a new value as well as the last blockchain to the blockchain
+    
+    Arguments:
+        :transaction_amount: The amount that should be added.
+        :last_transaction: The last blockchain transaction (defatult [1])
+    """
     blockchain.append([last_transaction, transaction_amount])
 
-tx_amount = float(input('Your transaction amount please: '))
+def get_user_input():
+    """ Returns the input of the user (a new transactions amount) as a float."""
+    user_input = float(input('Your transaction amount please: '))
+    return user_input
+
+tx_amount = get_user_input()
 add_value(tx_amount)
 
-tx_amount = float(input('Your transaction amount please: '))
+tx_amount = get_user_input()
 add_value(last_transaction=get_last_blockchain_value(),transaction_amount=tx_amount)
 
-tx_amount = float(input('Your transaction amount please: '))
+tx_amount = get_user_input()
 add_value(tx_amount, get_last_blockchain_value())
 
 print(blockchain)
